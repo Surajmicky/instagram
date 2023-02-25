e5b3cd097007680dc9c45e130b4ae6042461b5ff
Suraj kumar <surajmicky10@gmail.com>
2023-02-25 21:00:34 +0530

    created Navbar and header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+const Header = ({ innerWidth, filteredData, setFilteredData }) => {
+  return innerWidth > 480 ? (
+    <Sidebar
+      filteredData={filteredData}
+      setFilteredData={setFilteredData}
+      innerWidth={innerWidth}
+    />
+  ) : (
+    <Navbar
+      filteredData={filteredData}
+      setFilteredData={setFilteredData}
+      innerWidth={innerWidth}
+    />
+  );
+};
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { MdFavoriteBorder } from "react-icons/md";
+import React, { useState } from "react";
+import Searchbar from "./Searchbar";
 
-const Navbar = () => {
+const Navbar = ({ filteredData, setFilteredData }) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full sticky top-0 bg-white py-3 flex items-center justify-around">
+      <div className="w-16 pl-1">
+        <img
+          src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.svg"
+          alt="Instagram"
+        />
+      </div>
+      <Searchbar
+        filteredData={filteredData}
+        setFilteredData={setFilteredData}
+      />
+      <div>
+        <MdFavoriteBorder
+          size={30}
+          className="hover:border border-white cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
